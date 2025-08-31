3704fa3a9e2eab68a8f366547d736510ee30c08a
Joenatan <Joenatan506@gmail.com>
2025-08-31 21:46:45 +0700

    add write file

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -24,22 +24,5 @@ def get_files_info(working_directory, directory="."):
     except Exception as e:
         return f"Error listing files: {e}"
 
-def get_file_content(working_directory, file_path):
-    abs_full_path = os.path.abspath(os.path.join(working_directory, file_path))
-    abs_working_dir = os.path.abspath(working_directory)
-    if not abs_full_path.startswith(abs_working_dir):
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    
-    if not os.path.isfile(abs_full_path):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-    
-    try:
-        MAX_CHARS = 10000
 
-        with open(abs_full_path, "r") as f:
-            file_content_string = f.read(MAX_CHARS)
-            file_content_string += f'[...File "{file_path}" truncated at 10000 characters].'
-            return file_content_string
-    except Exception as e:
-        return f"Error: {e}"
 
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
-from functions.get_files_info import get_files_info, get_file_content
-
+from functions.get_files_info import get_files_info
+from functions.write_file import write_file
+from functions.get_file_content import get_file_content
 
 def test():
     result = get_files_info("calculator", ".")
@@ -42,7 +43,15 @@ def test2():
     print(result)
 
  
-    
+def test3():
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print(result)
 
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print(result)
+
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print(result)
+    
 if __name__ == "__main__":
-    test2()
+    test3()
