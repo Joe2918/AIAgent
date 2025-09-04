a96845a51b94ade37ef88017a611e87a9a8aa639
Joenatan <Joenatan506@gmail.com>
2025-09-04 14:49:28 +0700

    add calling function, agent/updating messages and loops

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
+from config import MAX_ITERS
 from prompts import system_prompt
-from call_function import available_functions
-
+from call_function import available_functions, call_function
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+from functions.write_file import write_file
+from functions.run_python import run_python_file
 def main():    
     load_dotenv()
 
@@ -35,8 +39,21 @@ def main():
     if verbose:
         print(f"User prompt: {user_prompt}")
 
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
 
-    generate_content(client, messages, verbose)
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
 
 
 def generate_content(client, messages, verbose):
@@ -46,20 +63,45 @@ def generate_content(client, messages, verbose):
         config=types.GenerateContentConfig(
             tools =[available_functions], system_instruction=system_prompt)
     )
+
+    if response.candidates:
+        for candidate in response.candidates:
+            function_call_content = candidate.content
+            messages.append(function_call_content)
+
+
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
     if verbose:
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
-    print("Response:")
-    
+
     if not response.function_calls:
         return response.text
 
+    function_responses = []
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+        messages.append(
+    types.Content(
+        role="user",
+        parts=[types.Part(text=str(item)) for item in function_responses]
+    )
+)
 
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
     
+    messages.append(types.Content(role="user", parts=function_responses))
 
 if __name__ == "__main__":
     main()
--- prompts.py
+++ prompts.py
@@ -3,10 +3,12 @@ You are a helpful AI coding agent.
 
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
-- List files and directories
-- Read file contents
-- Execute Python files with optional arguments
-- Write or overwrite files
+- List files and directories - by specifically saying get_files_info()
+- Read file contents - by specifically saying get_file_content()
+- Execute Python files with optional arguments - by specifically saying run_python_file()
+- Write or overwrite files - by specifically saying write_file()
+
+You have to specifically say the function correctly, you can check your available_functions user provided
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
